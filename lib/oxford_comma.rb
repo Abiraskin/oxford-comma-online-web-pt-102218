ac4cb28ae5aacd2ee24da48b0e0ec1c72567b308
Abi Raskin <abiraskin@gmail.com>
2018-10-28 17:05:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,9 @@
 def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
+  else
+    array.length 2 < array.legnth
+   array[-1].insert(0, "and ")
   end
   array.join(", ")
 end
