5975c922c20492661608ddc556b43f1b67898a55
Abi Raskin <abiraskin@gmail.com>
2018-10-28 17:06:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   elseif
     array.length 2 < array.legnth
-   array[-1].insert("and ")
+   array[-1].insert(0, "and ")
   end
   array.join(", ")
 end
